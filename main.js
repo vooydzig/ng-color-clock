abc0582a1353bca306558ab68ad32b496c4aacfb
vooydzig <vooydzig@gmail.com>
2015-11-14 01:28:53 +0100

    added optional properties
    * format -> sets date/time format
    * properties -> sets css property that will be modified

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
 var app = angular.module("clock", []);
 
 app.directive('ngColorClock', ['$interval', 'dateFilter', function ($interval, dateFilter) {
-    return {
-      restrict: 'E',
-        link: function (scope, element, attrs) {
-            var timeoutId;
+  function getColor(value, max) { return Math.floor(255*(value / max)); }
+  function toHex(value) { return value < 16 ? '0' + value.toString(16) : value.toString(16); }
+  return {
+    restrict: 'E',
+    scope : {
+      format: '@format',
+      properties: '@properties'
+    },
+    link: function (scope, element, attrs) {
+      var timeoutId;
+      var format = scope.format || "hh:mm:ss a";
+      var props = scope.properties ? scope.properties.split(' ') : ['background-color'];
 
-            element.addClass('color-clock');
-            update();
+      element.addClass('color-clock');
+      update();
 
-            function getColor(value, max) {
-                return Math.floor(255*(value / max));
-            }
+      function update() {
+        var date = new Date();
+        var r = getColor(date.getHours(), 23);
+        var g = getColor(date.getMinutes(), 59);
+        var b = getColor(date.getSeconds(), 59);
 
-            function update() {
-              var date = new Date();
-              var r = getColor(date.getHours(), 23);
-              var g = getColor(date.getMinutes(), 59);
-              var b = getColor(date.getSeconds(), 59);
-              element.text(dateFilter(date, 'hh:mm:ss a'));
-              element.css('background-color', "rgb("+r+", "+g+", "+b+")");
-            }
+        if (format.toLowerCase() === '#rgb') {
+          var rgb = toHex(r) + toHex(g) + toHex(b);
+          element.text(format === '#rgb' ? rgb.toLowerCase() : rgb.toUpperCase());
+        } else
+          element.text(dateFilter(date, format));
+        for (var i=0; i< props.length; i++)
+          element.css(props[i], "rgb("+r+", "+g+", "+b+")");
+      }
 
-            element.on('$destroy', function() {
-              $interval.cancel(timeoutId);
-            });
+      element.on('$destroy', function() {
+        $interval.cancel(timeoutId);
+      });
 
-
-            timeoutId = $interval(function() {
-              update();
-            }, 1000);
-        }
-    };
+      timeoutId = $interval(function() {
+        update();
+      }, 1000);
+    }
+  };
 }]);
